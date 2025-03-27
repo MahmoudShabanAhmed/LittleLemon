4ec2a0a373885f09702518e8d1dc68b5a5aa6dd1
Mahmoud Shaaban Ahmed <54320987+MahmoudShabanAhmed@users.noreply.github.com>
2025-03-27 23:31:42 +0200

    Add static files and url patterns

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -112,6 +112,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'resturant/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

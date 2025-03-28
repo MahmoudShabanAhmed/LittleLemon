fa102b81b707fc5fc85e759fdbdb4aa6ebe49403
Mahmoud Shaaban Ahmed <54320987+MahmoudShabanAhmed@users.noreply.github.com>
2025-03-28 04:46:17 +0200

    Added Booking endpoints and modify defualt router

--- littlelemon/resturant/urls.py
+++ littlelemon/resturant/urls.py
@@ -1,7 +1,9 @@
-from django.contrib import admin 
-from django.urls import path 
-from . import views 
-  
-urlpatterns = [ 
-    path('', views.index, name='index'), 
+from django.urls import path
+from resturant import views
+
+urlpatterns = [
+    path('menu/', views.MenuItemsView.as_view(), name='menu-list'),
+    path('menu/<int:pk>/', views.SingleMenuItemView.as_view(), name='menu-detail'),
+    path('booking/', views.BookingViewSet.as_view({'get': 'list', 'post': 'create'}), name='booking-list'),
+    path('booking/<int:pk>/', views.BookingViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='booking-detail'),
 ]
--- littlelemon/resturant/views.py
+++ littlelemon/resturant/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
+from resturant.serializers import UserSerializer
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from resturant.models import Menu, Booking
+from resturant.serializers import MenuSerializer, BookingSerializer
 
-# Create your views here.
-def index(request):
-    return render(request, 'index.html', {})
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+    
+class MenuItemsView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+   queryset = Booking.objects.all()
+   serializer_class = BookingSerializer
+   permission_classes = [IsAuthenticated] 
